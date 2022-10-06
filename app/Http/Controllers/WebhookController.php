9147cd919785ca6f47f43b9b78a9eb30b741ec01
eliphas <eliphas.sn@gmail.com>
2022-10-06 21:02:15 +0300

    Cron_test

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Calendar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -9,7 +10,21 @@ class WebhookController extends Controller
 {
     public function index(Request $request): \Illuminate\Http\JsonResponse
     {
-        Log::debug($request->all());
+        $data_request = explode('|', $request->input('callback_query')['data']);
+        $action = $data_request[0];
+        $id = $data_request[1];
+        $lesson = Calendar::where('id', $id)->first();
+
+        switch ($action){
+            case "1":
+                $lesson->status = 0;
+                $lesson->save();
+                break;
+            case "2":
+                $lesson->status = 3;
+                $lesson->save();
+                break;
+        }
         return response()->json(true, 200);
     }
 }
