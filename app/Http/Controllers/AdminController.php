2be9857482db9656bb361a877a7e5a56574384d6
eliphas <eliphas.sn@gmail.com>
2022-10-06 17:52:15 +0300

    Cron_test

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
     public function crone()
     {
         $calendar = Calendar::first();
-        $http = Http::get('https://api.tlgr.org/bot/'.env('REPORT_TELEGRAM_ID') .config('bots.bot') . '/setWebhook?url=https://new.it-schoollearn.com/crone');
+        $http = Http::get('https://api.tlgr.org/bot'.env('REPORT_TELEGRAM_ID') .config('bots.bot') . '/setWebhook?url=https://new.it-schoollearn.com/crone');
         dd(json_decode($http->body()));
         event(new LessonStart($calendar));
     }
