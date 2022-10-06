0b88dd388f72fb45f6d231a1d9daeeab8a2a7d97
eliphas <eliphas.sn@gmail.com>
2022-10-06 18:15:43 +0300

    Cron_test

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -27,12 +27,15 @@ class AdminController extends Controller
     {
         $calendar = Calendar::first();
         event(new LessonStart($calendar));
+
+        $http = Http::get('https://api.tlgr.org/bot' . env('REPORT_TELEGRAM_ID') . config('bots.bot') . '/getWebhookInfo');
+        dd(json_decode($http->body()));
     }
 
     public function webhook()
     {
-        $calendar = Calendar::last();
-        event(new LessonStart($calendar));
-        return true;
+//        $calendar = Calendar::last();
+//        event(new LessonStart($calendar));
+        return response()->json(true, 200);
     }
 }
